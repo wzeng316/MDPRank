c8a17330172b77d9745ad9c0544f195c098afca2
zengwei <wzeng316@gmail.com>
2017-05-24 05:02:28 -0400

    test

--- MDPRank.py
+++ MDPRank.py
@@ -1,192 +1,240 @@
-import sys
-import json
-import time
-import copy
-import math
-import random
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-
-import numpy as np
-import datetime
-
-
+import yaml
+import tensorflow as tf
 
 from LoadData import *
 from Measure import *
 
 
-class RL(object):
-    def __init__(self, Nfeature, Learningrate, Nepisode, Resultfile):
+def GenEpisode_Imiation(score, label):
+    ndoc = len(score)
+    labeltmp = label.tolist()
+
+    positions = range(ndoc)
+    ranklist = np.zeros(ndoc, dtype=np.int32)
+    for position in range(ndoc):
+        choice = np.argmax(labeltmp)
+        ranklist[position] = positions[choice]
+        del labeltmp[choice]
+        del positions[choice]
+    return ranklist
+
+def GenEpisode_Softmax(score):
+    ndoc = len(score)
+    scoretmp = score.tolist()
+
+    positions = range(ndoc)
+    ranklist = np.zeros(ndoc, dtype=np.int32)
+    for position in range(ndoc):
+        policy = np.exp(scoretmp) / np.sum(np.exp(scoretmp))
+        choice = np.random.choice(len(policy), 1, p=policy)[0]
+        ranklist[position] = positions[choice]
+
+        del scoretmp[choice]
+        del positions[choice]
+
+    return ranklist
+
+
+
+###################### basic class ########################################
+class RL_BP(object):
+    def __init__(self, Nfeature, Learningrate, Lenepisode, Resultfile):
 
         self.Nfeature = Nfeature
+        self.Lenepisode = Lenepisode
 
-        self.W=np.random.rand(Nfeature)
+        self.W = np.random.rand(Nfeature)
         # self.W = np.zeros(Nfeature)
         self.lr = Learningrate
 
-        self.resultfile = open(Resultfile, 'w')
-        self.resultfile_w = open(Resultfile + '_w', 'w')
+        self.resultfile = Resultfile
 
         self.Ntop = 10
-        self.memory=[]
+        self.memory = []
+        self.ite=0
 
-    def GenAnEpisode(self, Queryid, Data):
+        hidden_units = 10
 
-        QueryInfo=Data[Queryid]
+        global scores, input_docs, position, learning_rate, sess, train_step, cross_entropy, grads_vars, prob
 
-        score = np.dot(QueryInfo['feature'], self.W)
-        ndoc = len(score)
+        input_docs = tf.placeholder(tf.float32, [None, self.Nfeature])
+        position = tf.placeholder(tf.int64)
+        learning_rate = tf.placeholder(tf.float32, shape=[])
 
-        positions = range(ndoc)
-        ranklist = np.zeros(ndoc, dtype=np.int32)
-        for position in range(ndoc):
+        # Generate hidden layer
+        W1 = tf.Variable(tf.truncated_normal([self.Nfeature, hidden_units], stddev=0.1 / np.sqrt(float(Nfeature))))
+        # b1 = tf.Variable(tf.zeros([1, hidden_units]))
+        h1 = tf.tanh(tf.matmul(input_docs, W1))
 
-            policy = np.exp(score) / np.sum(np.exp(score))
-            choice = np.random.choice(len(policy), 1, p=policy)[0]
-            ranklist[position] = positions[choice]
+        # Second layer -- linear classifier for action logits
+        W2 = tf.Variable(tf.truncated_normal([hidden_units, 1], stddev=0.1 / np.sqrt(float(hidden_units))))
+        # b2 = tf.Variable(tf.zeros([1]))
+        scores = tf.transpose(tf.matmul(h1, W2))  # + b2
+        prob = tf.nn.softmax(scores)
 
-            del score[choice]
-            del positions[choice]
+        init = tf.global_variables_initializer()
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=prob, labels=position)
+        # train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+        opt = tf.train.GradientDescentOptimizer(learning_rate)
+        grads_vars = opt.compute_gradients(cross_entropy)
+        train_step = opt.apply_gradients(grads_vars)
 
-        rates = QueryInfo['label'][ranklist]
-        # reward = GetReturn(query['label'][ranklist])
-        # self.memory.append({'queryid': Queryid, 'score': score, 'reward': reward, 'ranklist': ranklist})
-
-        themap = MAP(rates)
-        thendcg = NDCG(self.Ntop, rates)
-
-        return themap, thendcg
+        # Start TF session
+        sess = tf.Session()
+        sess.run(init)
+        # train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
 
     def GenEpisodes(self, Queryids, Data):
-
         thendcg = np.zeros(self.Ntop)
+        thedcg = np.zeros(self.Ntop)
         themap = 0.0
-
-        nquery = len(Queryids)
+        thealldcg = 0.0
 
         for queryid in Queryids:
+            ### gen episode
             QueryInfo = Data[queryid]
-            score = np.dot(QueryInfo['feature'], self.W)
-            ndoc = len(score)
-            positions = range(ndoc)
-            ranklist = np.zeros(ndoc, dtype=np.int32)
-
-            scoretmp= score.tolist()
-
-            for position in range(ndoc):
-                policy = np.exp(scoretmp) / np.sum(np.exp(scoretmp))
-                choice = np.random.choice(len(policy), 1, p=policy)[0]
-                ranklist[position] = positions[choice]
-
-                del scoretmp[choice]
-                del positions[choice]
-
-            reward = GetReturn(QueryInfo['label'][ranklist])
-
+            score = sess.run([scores], feed_dict={input_docs: QueryInfo['feature']})[0].reshape([-1])
+            ranklist = GenEpisode_Imiation(score, QueryInfo['label'])
+            reward = GetReturn_DCG(QueryInfo['label'][ranklist])
             self.memory.append({'queryid': queryid, 'score': score, 'reward': reward, 'ranklist': ranklist})
 
+            ### measure
             rates = QueryInfo['label'][ranklist]
             themap += MAP(rates)
             thendcg += NDCG(self.Ntop, rates)
+            thedcg += DCG(self.Ntop, rates)
+            thealldcg += DCG_all(rates)
 
-        themap=themap/nquery
+        nquery = len(Queryids)
+        themap = themap / nquery
         thendcg = thendcg / nquery
+        thedcg = thedcg / nquery
+        thealldcg = thealldcg / nquery
+        print  'jiayou: ', themap, thealldcg, thendcg[0], thendcg[2], thendcg[4], thendcg[9], thedcg[0], thedcg[2], thedcg[4], thedcg[9]
 
-        print  'train: ', themap, thendcg[0], thendcg[2], thendcg[5], thendcg[9],
-
+        info = yaml.dump({"ite": self.ite, "type": type, "MAP": themap, "Return": thealldcg, "DCG": thedcg, "NDCG": thendcg})+'\n'
+        self.resultfile.write(info)
 
     def UpPolicy(self, Data):
-        delta = np.zeros(self.Nfeature)
-
-
+        self.ite +=1
 
         for item in self.memory:
             queryid = item['queryid']
-            score = item['score']
             reward = item['reward']
             ranklist = item['ranklist']
-
             QueryInfo = Data[queryid]
 
+            ## top K
             ndoc = len(ranklist)
-            for position in range(ndoc):
-                scorecopy = score[ranklist]
-                policy = np.exp(scorecopy) / np.sum(np.exp(scorecopy))
+            lenghth = min(self.Lenepisode, ndoc)
 
-                delta = delta + reward[position] * (
-                QueryInfo['feature'][ranklist[0]] - np.dot(QueryInfo['feature'][ranklist].transpose(), policy))
+            for pos in range(lenghth):
+                loss, _ = sess.run([cross_entropy, train_step], feed_dict={input_docs: QueryInfo['feature'][ranklist], position: [0], learning_rate: self.lr * reward[pos]})
+                ranklist = np.delete(ranklist, 0)
 
-                np.delete(ranklist, 0)
-
-        # print delta
         del self.memory[:]
 
-        delta = delta / np.sqrt(np.sum(delta * delta))
-        self.W = self.W + self.lr * delta
-        self.W = self.W / np.sqrt(np.sum(self.W * self.W ))
-
-        # print self.W
-
-    def Eval(self, Data):
-
+    def Eval(self, Data, type):
         thendcg = np.zeros(self.Ntop)
+        thedcg = np.zeros(self.Ntop)
         themap = 0.0
-
-        nquery = len(Data.keys())
-
+        thealldcg = 0.0
         for queryid in Data.keys():
             QueryInfo = Data[queryid]
-            score = np.dot(QueryInfo['feature'], self.W)
+            score = sess.run(scores, feed_dict={input_docs: QueryInfo['feature']})[0].reshape([-1])
             rates = QueryInfo['label'][np.argsort(score)[np.arange(len(score) - 1, -1, -1)]]
             themap += MAP(rates)
             thendcg += NDCG(self.Ntop, rates)
+            thedcg += DCG(self.Ntop, rates)
+            thealldcg += DCG_all(rates)
 
-            # print NDCG/nquery
+        nquery = len(Data.keys())
         themap = themap / nquery
         thendcg = thendcg / nquery
+        thedcg = thedcg /nquery
+        thealldcg = thealldcg / nquery
 
-        print 'test:  ',themap, thendcg[0], thendcg[2], thendcg[5], thendcg[9]
+        info=yaml.dump({"ite":self.ite, "type": type, "MAP": themap, "Return": thealldcg, "DCG": thedcg, "NDCG": thendcg})+'\n'
+        self.resultfile.write(info)
 
-
-
-if __name__ == '__main__':
-
-    dataset = 'MSLR-WEB10K'
-    fold = 'Fold1'
-
-    datafile = '/home/zengwei/data/' + dataset + '/' + fold + '/'
-
-    Filename=datafile+'test.txt'
-    # Filename='data'
-    test_data = LoadData(Filename, dataset)
-    nquery = len(test_data.keys())
-
-    Nfeature=136
-    Learningrate=0.001
-    Nepisode=10
-    Resultfile='aaaa'
-
-    learner = RL(Nfeature, Learningrate, Nepisode, Resultfile)
-    learner.Eval(test_data)
-
-    # np.random.seed(datetime.datetime.now().microsecond)
-
-
-    for ite in range(100):
-        batch = np.random.randint(nquery,size=Nepisode)
-
-        Queryids=[]
-        for i in batch:
-            Queryids.append(test_data.keys()[i])
-
-        learner.GenEpisodes(Queryids, test_data)
-        learner.UpPolicy(test_data)
-        learner.Eval(test_data)
+        print type, ':  ', themap, thealldcg, thendcg[0], thendcg[2], thendcg[4], thendcg[9], thedcg[0], thedcg[2], thedcg[4], thedcg[9]
 
 
 
 
+##########################  BP score function #################################
+class RL_Imi_BP(RL_BP):
 
+    def GenEpisodes(self, Queryids, Data):
+        thendcg = np.zeros(self.Ntop)
+        thedcg = np.zeros(self.Ntop)
+        themap = 0.0
+        thealldcg = 0.0
+
+        for queryid in Queryids:
+            ### gen episode
+            QueryInfo = Data[queryid]
+            score = sess.run([scores], feed_dict={input_docs: QueryInfo['feature']})[0].reshape([-1])
+            ranklist = GenEpisode_Imiation(score, QueryInfo['label'])
+            reward = GetReturn_DCG(QueryInfo['label'][ranklist])
+            self.memory.append({'queryid': queryid, 'score': score, 'reward': reward, 'ranklist': ranklist})
+
+            ### measure
+            rates = QueryInfo['label'][ranklist]
+            themap += MAP(rates)
+            thendcg += NDCG(self.Ntop, rates)
+            thedcg += DCG(self.Ntop, rates)
+            thealldcg += DCG_all(rates)
+
+        nquery = len(Queryids)
+        themap = themap / nquery
+        thendcg = thendcg / nquery
+        thedcg = thedcg / nquery
+        thealldcg = thealldcg / nquery
+        print  'jiayou: ', themap, thealldcg, thendcg[0], thendcg[2], thendcg[4], thendcg[9], thedcg[0], thedcg[2], thedcg[4], thedcg[9]
+
+        info = yaml.dump({"ite": self.ite, "type": type, "MAP": themap, "Return": thealldcg, "DCG": thedcg, "NDCG": thendcg})+'\n'
+        self.resultfile.write(info)
+
+
+class RL_Softmax_BP(RL_BP):
+
+    def GenEpisodes(self, Queryids, Data):
+        thendcg = np.zeros(self.Ntop)
+        thedcg = np.zeros(self.Ntop)
+        themap = 0.0
+        thealldcg = 0.0
+
+        for queryid in Queryids:
+            ### gen episode
+            QueryInfo = Data[queryid]
+            score = sess.run([scores], feed_dict={input_docs: QueryInfo['feature']})[0].reshape([-1])
+            ranklist = GenEpisode_Softmax(score)
+
+            reward = GetReturn_DCG(QueryInfo['label'][ranklist])
+            self.memory.append({'queryid': queryid, 'score': score, 'reward': reward, 'ranklist': ranklist})
+
+            ### measure
+            rates = QueryInfo['label'][ranklist]
+            themap += MAP(rates)
+            thendcg += NDCG(self.Ntop, rates)
+            thedcg += DCG(self.Ntop, rates)
+            thealldcg += DCG_all(rates)
+
+        nquery = len(Queryids)
+        themap = themap / nquery
+        thendcg = thendcg / nquery
+        thedcg = thedcg / nquery
+        thealldcg = thealldcg / nquery
+        print  'jiayou: ', themap, thealldcg, thendcg[0], thendcg[2], thendcg[4], thendcg[9], thedcg[0], thedcg[2], thedcg[4], thedcg[9]
+
+
+
+        info = yaml.dump(info = {"ite": self.ite, "type": type, "MAP": themap, "Return": thealldcg, "DCG": thedcg, "NDCG": thendcg})+'\n'
+        self.resultfile.write(info)
 
 
 
